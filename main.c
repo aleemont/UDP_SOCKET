accbfb0bb6679d76b576c9f2f69e7942a55a92db
Alessandro Monticelli <64771662+aleemont@users.noreply.github.com>
2020-12-28 15:14:28 +0100

    Delete main.c
    Useless file removed

--- main.c
+++
@@ -1,8 +0,0 @@
-/*
-	Utilizzo:
-	[1]Apri una nuova shell(ctrl+shift+s)
-	[2]Nella prima shell: cd /server
-	[3]Nella seconda shell: cd /client
-	[4]Nella prima shell: ./server
-	[5]Nella seconda shell: ./client
-*/
